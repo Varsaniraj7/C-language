5cc60d0aec2b3997849a8510ae3c10498ecedbf8
varsaniraj7 <varsaniraj7@gamil.co>
2024-05-23 19:51:11 +0530

    commited

---
+++ Weekly Task 4/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      screens: {
+        '2xl': '1440px',
+      },
+    },
+  },
+  plugins: [],
+}
+
